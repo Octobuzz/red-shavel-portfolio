ea1c3f6a20bba5fc75074381a10a62e9d99b0c1a
Ольга Пастухова <olg.pastukhova1@x5.ru>
2021-02-06 20:40:04 +0300

    дкинуты файлы

---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="footer">
+		<ul class="footer-list">
+			<li class="footer-item">
+				<a
+					class="footer-link twitter"
+					href="#"
+				/>
+			</li>
+			<li class="footer-item">
+				<a
+					class="footer-link discord"
+					href="#"
+				/>
+			</li>
+			<li class="footer-item">
+				<a
+					class="footer-link gmail"
+					href="#"
+				/>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class Footer extends Vue {
+}
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/views/Main.vue
@@ -0,0 +1,83 @@
+<template>
+	<div class="main">
+		<div class="main-section">
+			<h1 class="main-title">
+				Live2D Rigging Commission
+			</h1>
+			<p class="main-text">
+				Hi, I'm Nick. I create live2D model for Vtubers :)
+			</p>
+		</div>
+		<div class="main-section">
+			<div class="main-info">
+				<h2 class="main-title">
+					Live2D Showcase
+				</h2>
+				<p class="main-text">
+					Please contact me to see more.
+				</p>
+				<a
+					class="main-link"
+					href="#"
+				>More</a>
+			</div>
+			<div class="main-img">
+				<img
+					src="#"
+					alt=""
+				/>
+			</div>
+		</div>
+		<div class="main-section">
+			<div class="main-info">
+				<h2 class="main-title">
+					Price List
+				</h2>
+				<p class="main-text">
+					A bust-up model starts at $360
+				</p>
+				<a
+					class="main-link"
+					href="#"
+				>More</a>
+			</div>
+			<div class="main-img">
+				<img
+					src="#"
+					alt=""
+				/>
+			</div>
+		</div>
+		<div class="main-section">
+			<div class="main-info">
+				<h2 class="main-title">
+					Terms of Service
+				</h2>
+				<p class="main-text">
+					Please read before commissioning.
+				</p>
+				<a
+					class="main-link"
+					href="#"
+				>More</a>
+			</div>
+			<div class="main-img">
+				<img
+					src="#"
+					alt=""
+				/>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class Home extends Vue {
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
