2e29a3251bd7fffc1e04edaa3af2d2444059a835
Никита Павленко <nick.pavlenko1998@gmail.com>
2021-02-06 20:42:57 +0300

    добавлен файл миксинов

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,26 @@
+
+const path = require('path');
+
 module.exports = {
 	lintOnSave: false,
+	chainWebpack: config => {
+		config.resolve.alias.set('@', path.resolve(__dirname, 'src'));
+		config.module.rule('downloads')
+		// bundle common document files
+			.test(/\.(zip)(\?.*)?$/)
+			.use('file-loader')
+		  // use the file-loader
+		  	.loader('file-loader')
+		  // bundle into the "downloads" directory
+		 	.options({ name: 'downloads/[name].[hash:8].[ext]' });
+	},
+	css: {
+		loaderOptions: {
+			sass: {
+				prependData: `
+        @import "@/assets/styles/global.scss";
+        `
+			}
+		}
+	}
 };
